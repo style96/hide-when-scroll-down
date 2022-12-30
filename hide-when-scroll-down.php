a133bacb471fcfb7b5e6038b71ad868bfd434606
Halil <style93@gmail.com>
2022-12-30 17:41:43 +0300

    init files

---
+++ hide-when-scroll-down.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name: Sticky Header & Hide When Scroll Down
+ * Plugin URI: http://example.com/hello-world
+ * Description: A plugin that hides header on scroll down and appear on scroll up.
+ * Version: 1.0
+ * Author: Halil Sen
+ * Author URI: https://kodlamayabasla.com
+ * License: GPLv2 or later
+ * 
+ * Copyright 2022 KodlamayaBasla
+ * 
+ * KodlamayaBasla is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version. 
+ * 
+ * KodlamayaBasla is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with KodlamayaBasla. 
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ function hide_when_scroll_down() {
+    $my_file = plugins_url( '/assets/css/hide-header-when-scroll.css', __FILE__ );
+	wp_register_style('hide_when_scroll_down_header_plugin_css', $my_file);
+	wp_enqueue_style('hide_when_scroll_down_header_plugin_css');
+
+	$my_file = plugins_url( '/assets/js/hide_navbar_scrolldown.js', __FILE__ );
+	wp_register_script('hide_when_scroll_down_header_plugin_js', $my_file, array(), null, true);
+	wp_enqueue_script('hide_when_scroll_down_header_plugin_js');
+}
+add_action('wp_enqueue_scripts', 'hide_when_scroll_down', 999);
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Sticky Header & Hide When Scroll Down ===
+Contributors: style96
+Tags: sticky header, hide scroll down, hide, header, hide header, hide sticky header, scroll
+Donate link: https://example.com/
+Requires at least: 5.9
+Tested up to: 6.1
+Stable tag: 1.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Hide Sticky Header when you scroll down and appear when you scroll up again.
+
+== Description ==
+
+This plugin make your header sticky on top of page. Also add feature hide header when scroll down. Hide the amount of headers by pixels you scroll.
+This plugin only works with astra themes.
+== Installation ==
+
+1. Upload the "hide-when-scroll-down" directory to your "wp-content/plugins" directory.
+2. In your WordPress admin, go to PLUGINS and activate "Sticky Header & Hide When Scroll Down"
+3. Party!
+
+== Frequently Asked Questions ==
+
+= How do I use this plugin? =
+
+Just download and activate it.
+
+= How to uninstall the plugin? =
+ 
+Simply deactivate and delete the plugin.
+
+== Screenshots ==
+
+1. This screenshot shows hide header on scroll down and appear when scroll up again.
+2. This is when plugin is deactive.
+
+== Changelog ==
+
+= 1.0 =
+* Plugin released.
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+Plugin released.
